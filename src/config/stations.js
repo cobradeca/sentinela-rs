58f7704b7806a06ebd55df8caac9da11298f2c83
Andre Cobra <cobradeca@gmail.com>
2026-06-05 18:13:46 -0300

    Ajusta cidades do dashboard e previsao

--- src/config/stations.js
+++ src/config/stations.js
@@ -8,19 +8,47 @@ export const STATIONS_LAGOA = [
 ];
 
 export const STATIONS_CIDADES = [
-  { id: "rs_porto_alegre",  name: "Porto Alegre",    lat: -30.03, lon: -51.23, type: "cidade", ibgeCode: "4314902", rioRef: "Gua\u00edba \u2014 enchente mai/2024", demDensity: 2681 },
-  { id: "rs_canoas",        name: "Canoas",          lat: -29.92, lon: -51.18, type: "cidade", ibgeCode: "4304606", rioRef: "Lago Gua\u00edba / Gravata\u00ed", demDensity: 2654 },
-  { id: "rs_sao_leopoldo",  name: "S\u00e3o Leopoldo",    lat: -29.76, lon: -51.14, type: "cidade", ibgeCode: "4318705", rioRef: "Rio dos Sinos", demDensity: 2131 },
-  { id: "rs_lajeado",       name: "Lajeado",         lat: -29.47, lon: -51.96, type: "cidade", ibgeCode: "4311403", rioRef: "Rio Taquari \u2014 recorde 2023", demDensity: 1032 },
-  { id: "rs_caxias_sul",    name: "Caxias do Sul",   lat: -29.17, lon: -51.17, type: "cidade", ibgeCode: "4305108", rioRef: "Bacia do Ca\u00ed", demDensity: 282 },
-  { id: "rs_passo_fundo",   name: "Passo Fundo",     lat: -28.26, lon: -52.41, type: "cidade", ibgeCode: "4314100", rioRef: "Rio Passo Fundo", demDensity: 263 },
-  { id: "rs_pelotas",       name: "Pelotas",         lat: -31.77, lon: -52.34, type: "cidade", ibgeCode: "4314407", rioRef: "Canal S\u00e3o Gon\u00e7alo", demDensity: 202 },
-  { id: "rs_santa_maria",   name: "Santa Maria",     lat: -29.68, lon: -53.81, type: "cidade", ibgeCode: "4316907", rioRef: "Bacia do Vacaca\u00ed", demDensity: 153 },
-  { id: "rs_rio_grande",    name: "Rio Grande",      lat: -32.03, lon: -52.10, type: "cidade", ibgeCode: "4315602", rioRef: "Lagoa dos Patos / litoral", demDensity: 68 },
-  { id: "rs_cachoeira_sul", name: "Cachoeira do Sul",lat: -29.88, lon: -52.89, type: "cidade", ibgeCode: "4303004", rioRef: "Rio Jacu\u00ed", demDensity: 21 },
+  { id: "rs_porto_alegre",          name: "Porto Alegre",             lat: -30.03, lon: -51.23, type: "cidade", ibgeCode: "4314902", rioRef: "Gua\u00edba \u2014 enchente mai/2024", demDensity: 2681 },
+  { id: "rs_lajeado",               name: "Lajeado",                  lat: -29.47, lon: -51.96, type: "cidade", ibgeCode: "4311403", rioRef: "Rio Taquari \u2014 recorde 2023", demDensity: 1032 },
+  { id: "rs_guaiba",                name: "Gua\u00edba",                   lat: -30.11, lon: -51.32, type: "cidade", ibgeCode: "4309308", rioRef: "Gua\u00edba / margem oeste", demDensity: 169 },
+  { id: "rs_barra_ribeiro",         name: "Barra do Ribeiro",         lat: -30.29, lon: -51.30, type: "cidade", ibgeCode: "4301909", rioRef: "Gua\u00edba / Lagoa dos Patos", demDensity: 22 },
+  { id: "rs_tapes",                 name: "Tapes",                    lat: -30.67, lon: -51.40, type: "cidade", ibgeCode: "4321105", rioRef: "Lagoa dos Patos", demDensity: 21 },
+  { id: "rs_camaqua",               name: "Camaqu\u00e3",                  lat: -30.85, lon: -51.81, type: "cidade", ibgeCode: "4303509", rioRef: "BR-116 / Costa Doce", demDensity: 37 },
+  { id: "rs_arambare",              name: "Arambar\u00e9",                 lat: -30.91, lon: -51.50, type: "cidade", ibgeCode: "4300851", rioRef: "Lagoa dos Patos", demDensity: 8 },
+  { id: "rs_cristal",               name: "Cristal",                  lat: -31.00, lon: -52.05, type: "cidade", ibgeCode: "4306056", rioRef: "BR-116 / Camaqu\u00e3", demDensity: 7 },
+  { id: "rs_sao_lourenco_sul",      name: "S\u00e3o Louren\u00e7o do Sul",      lat: -31.36, lon: -51.98, type: "cidade", ibgeCode: "4318804", rioRef: "Lagoa dos Patos", demDensity: 22 },
+  { id: "rs_turucu",                name: "Turu\u00e7u",                   lat: -31.44, lon: -52.18, type: "cidade", ibgeCode: "4322327", rioRef: "BR-116 / Pelotas", demDensity: 12 },
+  { id: "rs_pelotas",               name: "Pelotas",                  lat: -31.77, lon: -52.34, type: "cidade", ibgeCode: "4314407", rioRef: "Canal S\u00e3o Gon\u00e7alo", demDensity: 202 },
+  { id: "rs_rio_grande",            name: "Rio Grande",               lat: -32.03, lon: -52.10, type: "cidade", ibgeCode: "4315602", rioRef: "Lagoa dos Patos / litoral", demDensity: 68 },
+  { id: "rs_sao_jose_norte",        name: "S\u00e3o Jos\u00e9 do Norte",        lat: -32.02, lon: -52.04, type: "cidade", ibgeCode: "4318507", rioRef: "Lagoa dos Patos / costa", demDensity: 22 },
+  { id: "rs_arroio_grande",         name: "Arroio Grande",            lat: -32.24, lon: -53.09, type: "cidade", ibgeCode: "4301305", rioRef: "BR-116 / Jaguar\u00e3o", demDensity: 7 },
+  { id: "rs_jaguarao",              name: "Jaguar\u00e3o",                 lat: -32.57, lon: -53.38, type: "cidade", ibgeCode: "4311007", rioRef: "BR-116 / fronteira", demDensity: 14 },
+  { id: "rs_santa_vitoria_palmar",  name: "Santa Vit\u00f3ria do Palmar", lat: -33.52, lon: -53.37, type: "cidade", ibgeCode: "4317301", rioRef: "BR-471 / extremo sul", demDensity: 5 },
+  { id: "rs_chui",                  name: "Chu\u00ed",                    lat: -33.69, lon: -53.46, type: "cidade", ibgeCode: "4305439", rioRef: "BR-471 / fronteira", demDensity: 30 },
+  { id: "rs_mostardas",             name: "Mostardas",                lat: -31.11, lon: -50.92, type: "cidade", ibgeCode: "4312500", rioRef: "BR-101 / Lagoa do Peixe", demDensity: 6 },
+  { id: "rs_tavares",               name: "Tavares",                  lat: -31.28, lon: -51.09, type: "cidade", ibgeCode: "4321352", rioRef: "BR-101 / Lagoa do Peixe", demDensity: 6 },
+  { id: "rs_palmares_sul",          name: "Palmares do Sul",          lat: -30.26, lon: -50.51, type: "cidade", ibgeCode: "4313656", rioRef: "BR-101 / litoral m\u00e9dio", demDensity: 10 },
+  { id: "rs_capivari_sul",          name: "Capivari do Sul",          lat: -30.14, lon: -50.52, type: "cidade", ibgeCode: "4304671", rioRef: "BR-101 / litoral", demDensity: 9 },
+  { id: "rs_osorio",                name: "Os\u00f3rio",                  lat: -29.89, lon: -50.27, type: "cidade", ibgeCode: "4313508", rioRef: "BR-101 / litoral norte", demDensity: 72 },
+  { id: "rs_tramandai",             name: "Tramanda\u00ed",               lat: -29.98, lon: -50.13, type: "cidade", ibgeCode: "4321600", rioRef: "Litoral norte", demDensity: 287 },
+  { id: "rs_torres",                name: "Torres",                   lat: -29.34, lon: -49.73, type: "cidade", ibgeCode: "4321501", rioRef: "BR-101 / divisa SC", demDensity: 215 },
 ];
 
-export const STATIONS = [...STATIONS_CIDADES];
+export const DASHBOARD_CITY_IDS = [
+  "rs_lajeado",
+  "rs_porto_alegre",
+  "rs_guaiba",
+  "rs_arambare",
+  "rs_sao_lourenco_sul",
+  "rs_pelotas",
+  "rs_rio_grande",
+  "rs_sao_jose_norte",
+  "rs_arroio_grande",
+];
+
+export const STATIONS = DASHBOARD_CITY_IDS
+  .map((id) => STATIONS_CIDADES.find((station) => station.id === id))
+  .filter(Boolean);
 export const ALL_STATIONS = [...STATIONS_LAGOA, ...STATIONS_CIDADES];
 
 export const APAS_RS = [
