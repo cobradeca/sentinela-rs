ce1456b1bd6e6e41636826e94560ab2f729c61f6
Andre Cobra <cobradeca@gmail.com>
2026-06-12 08:13:39 -0300

    Updates

--- assets/VooTab-BvLwmJJe.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ZoAdvTok.js";import{C as r}from"./CondicoesVoo-BRmGtvol.js";function t(){return o.jsx("div",{style:{display:"grid",gap:16},children:o.jsx(r,{})})}export{t as VooTab};
---
+++ assets/VooTab-BxyrwdDB.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CfUyEYHT.js";import{C as r}from"./CondicoesVoo-B_Pwh2ha.js";function t(){return o.jsx("div",{style:{display:"grid",gap:16},children:o.jsx(r,{})})}export{t as VooTab};
